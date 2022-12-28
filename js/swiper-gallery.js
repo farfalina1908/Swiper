f04d45da10f9484e2ce71def38f0464466adae86
Elena Berezina <farfalina1908@gmail.com>
2022-12-29 01:28:30 +0200

    update

--- js/swiper-gallery.js
+++ js/swiper-gallery.js
@@ -1,40 +1,44 @@
-new Swiper(".gallery-slider", {
-    // paginaion: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    //   dynamicBullets: true,
-    // },
-  
-    slideToClickedSlide: true,
+new Swiper('.gallery-slider', {
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+   },
+   paginaion: {
+     el: '.swiper-pagination',
+     clickable: true,
+     dynamicBullets: true,
+   },
 
-    hashNavigation: {
-      watchState: true,
-    },
-  
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-      pageUpDown: true,
-    },
-  
-    mousewheel: {
-      sensitivity: 1,
-      eventsTarget: ".gallery-slider",
-    },
-  
-    loop: true,
-    loopedSlides: 3,
-  
-    autoplay: {
-      delay: 1000,
-      stopOnLastSlide: false,
-      disableOnInteraction: false,
-    },
-  
-    speed: 1000,
-  
-    effect: "fade",
-    fadeEffect: {
-      crossFade: true,
-    },
-  });
+   // slideToClickedSlide: true,
+
+   // hashNavigation: {
+   //    watchState: true,
+   // },
+
+   // keyboard: {
+   //    enabled: true,
+   //    onlyInViewport: true,
+   //    pageUpDown: true,
+   // },
+
+   // mousewheel: {
+   //    sensitivity: 1,
+   //    eventsTarget: '.gallery-slider',
+   // },
+
+   // loop: true,
+   // loopedSlides: 3,
+
+   // autoplay: {
+   //    delay: 1000,
+   //    stopOnLastSlide: false,
+   //    disableOnInteraction: false,
+   // },
+
+   // speed: 1000,
+
+   // effect: "fade",
+   // fadeEffect: {
+   //    crossFade: true,
+   // },
+});
